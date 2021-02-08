2309e74645e673349b99084763e22428e75de6cd
Raúl Cátedra <raulcabe3003@gmail.com>
2021-02-08 17:27:26 +0100

    eliminate trash button

--- php/controllers/navController.php
+++ php/controllers/navController.php
@@ -1,5 +1,4 @@
 <?php
 echo "<ul><li class='navbar-title'><a href='index.php'>My Folders</a></li>";
 getAllFolderInRoot("");
-echo "<li class='navbar-trash'><a href='index.php?trash='><i class='bx bx-trash'></i>Trash<box-icon name='chevron-right'></box-icon></a>";
 echo "</ul>";
